9578b01332d0bbf93035bf0a73f71ce10f037fce
EmirDadanovic <56722228+EmirDadanovic@users.noreply.github.com>
2021-03-27 17:33:01 +0100

    Add files via upload

---
+++ Cjezbasharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cjezbasharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pjesma pjesma1 = new Pjesma("loose yourself", "eminem",500);
+            Pjesma pjesma2 = new Pjesma("unchained melody","righteous brothers",500);
+            Console.WriteLine(pjesma2.Brojpjesama());
+            Console.WriteLine(Pjesma.brojpjesama);
+             Console.ReadLine();
+        }
+
+       
+    }
+}
---
+++ Cjezbasharp/film.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cjezbasharp
+{
+    class Pjesma {
+        public string ime;
+        public string pjevac;
+        public int duzina;
+        public static int brojpjesama;
+
+        public Pjesma(string aime, string apjevac, int aduzina) 
+        {
+            ime = aime;
+            pjevac = apjevac;
+            duzina = aduzina;
+            brojpjesama++;  
+        }
+        public int Brojpjesama() {
+            return brojpjesama;
+        }
+    }
+        
+}
+    
+
+
